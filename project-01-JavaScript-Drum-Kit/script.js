c3f951ff14a2f409a1477f13aa93d17c60a5cc7b
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2023-04-21 22:06:41 +0300

    feat: add 'transitionend' event handling

--- project-01-JavaScript-Drum-Kit/script.js
+++ project-01-JavaScript-Drum-Kit/script.js
@@ -1,4 +1,4 @@
-window.addEventListener('keydown', function(e) {
+function keyDownHandler(e){
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`div.key[data-key="${e.keyCode}"]`);
 
@@ -10,5 +10,18 @@ window.addEventListener('keydown', function(e) {
     audio.play();
 
     key.classList.add("play");
-});
+};
+
+function removeTransitionHandler(e){
+    // skip it if it's not a transform
+    if(e.propertyName !== "transform") return;
+    this.classList.remove("play");
+}
+
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransitionHandler));
+window.addEventListener('keydown', keyDownHandler);
+
+
 
