046fe03c6def413fb133d9e419e4d47f6cf668ea
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2023-04-21 22:08:54 +0300

    refactor: apply formate document command

--- project-01-JavaScript-Drum-Kit/script.js
+++ project-01-JavaScript-Drum-Kit/script.js
@@ -1,27 +1,25 @@
-function keyDownHandler(e){
+function keyDownHandler(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`div.key[data-key="${e.keyCode}"]`);
 
     // for those keys, which are not supported stop running the function
-    if(!audio) return;
+    if (!audio) return;
 
     // rewind to the start
     audio.currentTime = 0;
     audio.play();
 
-    key.classList.add("play");
-};
-
-function removeTransitionHandler(e){
-    // skip it if it's not a transform
-    if(e.propertyName !== "transform") return;
-    this.classList.remove("play");
+    key.classList.add('play');
 }
 
+function removeTransitionHandler(e) {
+    // skip it if it's not a transform
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('play');
+}
 
 const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend', removeTransitionHandler));
+keys.forEach((key) =>
+    key.addEventListener('transitionend', removeTransitionHandler),
+);
 window.addEventListener('keydown', keyDownHandler);
-
-
-
