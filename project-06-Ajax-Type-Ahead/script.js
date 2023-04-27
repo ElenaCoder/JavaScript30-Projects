0a853c75d75b3d508d659d7cef693093a9e74131
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2023-04-27 13:34:02 +0300

    feat: add sorting by desc/asc order feature to project-06

--- project-06-Ajax-Type-Ahead/script.js
+++ project-06-Ajax-Type-Ahead/script.js
@@ -1,13 +1,21 @@
 const endpointURL =
     'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 
-const cities = [];
+const citiesSortedDesc = [];
+let citiesSortedAsc = [];
+
 const searchInput = document.querySelector('.search');
 const suggestions = document.querySelector('.suggestions');
+const arrow = document.querySelector('.arrow');
 
 fetch(endpointURL)
     .then((rawData) => rawData.json())
-    .then((data) => cities.push(...data));
+    .then((data) => {
+        citiesSortedDesc.push(...data);
+        citiesSortedAsc = citiesSortedDesc
+            .slice()
+            .sort((a, b) => +a.population - +b.population);
+    });
 
 function findMatches(wordToMatch, cities) {
     return cities.filter((place) => {
@@ -21,20 +29,26 @@ function numberWithCommas(x) {
 }
 
 function displayMatches() {
-    // console.log(this.value);
-    const matchArray = findMatches(this.value, cities);
-    // console.log(matchArray);
-    const regex = new RegExp(this.value, 'gi');
+    if(searchInput.value === '') return;
+
+    let matchArray;
+    if (arrow.classList.contains('desc')) {
+        matchArray = findMatches(searchInput.value, citiesSortedDesc);
+    } else if (arrow.classList.contains('asc')) {
+        matchArray = findMatches(searchInput.value, citiesSortedAsc);
+    }
+
+    const regex = new RegExp(searchInput.value, 'gi');
 
     let html = matchArray
         .map((place) => {
             const cityName = place.city.replace(
                 regex,
-                `<span class='highlight'>${this.value}</span>`,
+                `<span class='highlight'>${searchInput.value}</span>`,
             );
             const cityState = place.state.replace(
                 regex,
-                `<span class='highlight'>${this.value}</span>`,
+                `<span class='highlight'>${searchInput.value}</span>`,
             );
 
             return `
@@ -49,5 +63,21 @@ function displayMatches() {
     suggestions.innerHTML = html;
 }
 
+function arrowToggleHandling() {
+    const currentClassName = this.className;
+    if (currentClassName.includes('desc')) {
+        const newClassName = currentClassName.replace('desc', 'asc');
+        this.className = newClassName;
+        this.innerHTML = '&#8593;';
+    } else if (currentClassName.includes('asc')) {
+        const newClassName = currentClassName.replace('asc', 'desc');
+        this.className = newClassName;
+        this.innerHTML = '&#8595;';
+    }
+}
+
 searchInput.addEventListener('change', displayMatches);
 searchInput.addEventListener('keyup', displayMatches);
+
+arrow.addEventListener('click', arrowToggleHandling);
+arrow.addEventListener('click', displayMatches);
