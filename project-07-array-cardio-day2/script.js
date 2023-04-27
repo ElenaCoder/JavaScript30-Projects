9e2beec559540e74bb8dce0bbb31cb9d9c180614
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2023-04-27 15:40:36 +0300

    feat: add solution to task1-2 of project-07

--- project-07-array-cardio-day2/script.js
+++ project-07-array-cardio-day2/script.js
@@ -5,25 +5,36 @@ const people = [
     { name: 'Wes', year: 1988 },
     { name: 'Kait', year: 1986 },
     { name: 'Irv', year: 1970 },
-    { name: 'Lux', year: 2015 }
-  ];
+    { name: 'Lux', year: 2015 },
+];
 
-  const comments = [
+const comments = [
     { text: 'Love this!', id: 523423 },
     { text: 'Super good', id: 823423 },
     { text: 'You are the best', id: 2039842 },
     { text: 'Ramen is my fav food ever', id: 123523 },
-    { text: 'Nice Nice Nice!', id: 542328 }
-  ];
+    { text: 'Nice Nice Nice!', id: 542328 },
+];
 
-  // Some and Every Checks
-  // Array.prototype.some() // is at least one person 19 or older?
-  // Array.prototype.every() // is everyone 19 or older?
+// Some and Every Checks
+// 1. Array.prototype.some() // is at least one person 19 or older?
+const isAdult = people.some((person) => {
+    const currentYear = new Date().getFullYear();
+    return currentYear - person.year > 19;
+    0;
+});
+console.log('Task 1: ', isAdult);
 
-  // Array.prototype.find()
-  // Find is like filter, but instead returns just the one you are looking for
-  // find the comment with the ID of 823423
+//2. Array.prototype.every() // is everyone 19 or older?
+const isEveryAdult = people.every(
+    (person) => new Date().getFullYear() - person.year > 19,
+);
+console.log('Task 2: ', isEveryAdult);
 
-  // Array.prototype.findIndex()
-  // Find the comment with this ID
-  // delete the comment with the ID of 823423
+// Array.prototype.find()
+// Find is like filter, but instead returns just the one you are looking for
+// find the comment with the ID of 823423
+
+// Array.prototype.findIndex()
+// Find the comment with this ID
+// delete the comment with the ID of 823423
