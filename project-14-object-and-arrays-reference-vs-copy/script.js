94ef13c113a12619e796276dbb06bacf6ec10e6c
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2023-05-04 12:54:34 +0300

    feat: add solutions for strings, numbers, booleans

--- project-14-object-and-arrays-reference-vs-copy/script.js
+++ project-14-object-and-arrays-reference-vs-copy/script.js
@@ -1,29 +1,36 @@
 // start with strings, numbers and booleans
+console.groupCollapsed(`${'Task-1: strings, numbers and booleans'}`);
+console.groupCollapsed(`${'Task-1.1: strings'}`);
+let name1 = 'Elena';
+let name2 = name1;
+console.log('name1:', name1, 'name2:', name2);
+name1 = 'Lena';
+console.log('name1 was changed');
+console.log('Updated name1:', name1, 'Not affected name2:', name2);
+console.groupEnd();
 
-// Let's say we have an array
-const players = ['Wes', 'Sarah', 'Ryan', 'Poppy'];
+console.groupCollapsed(`${'Task-1.2: numbers'}`);
+let num1 = 20;
+let num2 = num1;
+console.log('num1:', num1, 'num2:', num2);
+num1 = 30;
+console.log('num1 was changed');
+console.log('Updated num1:', num1, 'Not affected num2:', num2);
+console.groupEnd();
 
-// and we want to make a copy of it.
+console.groupCollapsed(`${'Task-1.3: booleans'}`);
+let boolean1 = 20;
+let boolean2 = boolean1;
+console.log('boolean1:', boolean1, 'boolean2:', boolean2);
+boolean1 = 30;
+console.log('boolean1 was changed');
+console.log('Updated boolean1:', boolean1, 'Not affected boolean2:', boolean2);
+console.groupEnd();
+console.groupEnd();
 
-// You might think we can just do something like this:
+//-----------------------------------
 
-// however what happens when we update that array?
 
-// now here is the problem!
-
-// oh no - we have edited the original array too!
-
-// Why? It's because that is an array reference, not an array copy. They both point to the same array!
-
-// So, how do we fix this? We take a copy instead!
-
-// one way
-
-// or create a new array and concat the old one in
-
-// or use the new ES6 Spread
-
-// now when we update it, the original one isn't changed
 
 // The same thing goes for objects, let's say we have a person object
 
@@ -39,4 +46,5 @@ const person = {
 
 // We will hopefully soon see the object ...spread
 
-// Things to note - this is only 1 level deep - both for Arrays and Objects. lodash has a cloneDeep method, but you should think twice before using it.
+// Things to note - this is only 1 level deep - both for Arrays and Objects.
+//lodash has a cloneDeep method, but you should think twice before using it.
