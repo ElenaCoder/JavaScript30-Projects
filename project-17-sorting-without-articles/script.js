56a22041fbc40a60a7e7e6deb73527289bc2ad75
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2023-05-08 19:58:57 +0300

    feat: add alphabetical sorting by button clicking feature

--- project-17-sorting-without-articles/script.js
+++ project-17-sorting-without-articles/script.js
@@ -1,4 +1,4 @@
-const bands = [
+const initialBands = [
     'The Plot in You',
     'The Devil Wears Prada',
     'Pierce the Veil',
@@ -13,3 +13,28 @@ const bands = [
     'Anywhere But Here',
     'An Old Dog',
 ];
+
+const ul = document.querySelector('#bands');
+const button = document.querySelector('button');
+
+function stripArticle(bandName) {
+    return bandName.replace(/^(a |an |the )/i, '').trim();
+}
+
+function sortArr(arr = []) {
+    return Array.from(arr).sort((a, b) =>
+        stripArticle(a) > stripArticle(b) ? 1 : -1,
+    );
+}
+
+function displayBands(arr = initialBands) {
+    let html = arr.map((band) => `<li>${band}</li>`).join('');
+
+    ul.innerHTML = html;
+}
+
+displayBands();
+
+button.addEventListener('click', () => {
+    displayBands(sortArr(initialBands));
+});
